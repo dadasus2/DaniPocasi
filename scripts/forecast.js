85973b07f15369960b214bf2795d4bae7e50ac9e
Daniel <bohatec.daniel@stud.vassboskovice.cz>
2023-08-03 01:44:28 +0200

    .

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,12 +1,7 @@
 const key = "kFyHcl9yojt7NZlK1rl6OVhn9XATrwtb";
 
-
-
-
-
-
 const getWeather = async (cityId) => {
-    const link = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const link = "https://dataservice.accuweather.com/currentconditions/v1/";
     const query = `${cityId}?apikey=${key}&language=cs-cz`
 
     const output = await fetch(link + query)
@@ -17,7 +12,7 @@ const getWeather = async (cityId) => {
 }
 
 const getCity = async (city) => {
-    const link = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const link = "https://dataservice.accuweather.com/locations/v1/cities/search";
     const query = `?apikey=${key}&q=${city}`
 
     const output = await fetch(link + query)
